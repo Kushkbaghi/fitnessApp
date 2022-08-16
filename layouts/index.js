5e43f11d28a8da8aefcf2e3becdb41ca6dd99d52
Nick Kushkbaghi <nick.kushkbaghi@gmail.com>
2022-08-16 19:03:09 +0200

    First Commit, document, folder, files, settings are done

---
+++ layouts/index.js
@@ -0,0 +1,6 @@
+export { default as Nav } from './nav';
+export { default as Footer } from './footer';
+export { default as Header } from './header';
+export { default as section } from './section';
+export { default as article } from './article';
+export { default as aside } from './aside';
---
+++ styles/theme.js
@@ -0,0 +1,38 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#00bcd4',
+      light: '#00bcd4',
+      lighter: '#00bcd4',
+      dark: '#00bcd4',
+      darker: '#00bcd4',
+    },
+    secondary: {
+      main: '#00bcd4',
+      light: '#00bcd4',
+      lighter: '#00bcd4',
+      dark: '#00bcd4',
+      darker: '#00bcd4',
+    },
+    color: {
+      dark: '#00bcd4',
+      darker: '#00bcd4',
+      darkest: '#00bcd4',
+      light: '#00bcd4',
+      lighter: '#00bcd4',
+      lightest: '#00bcd4',
+    },
+  },
+  typography: {
+    fontFamily: [
+      '"Roboto"',
+      'Josefin Sans',
+      'Jost',
+      'Montserrat',
+      'Poiret One',
+      'sans-serif',
+    ].join(','),
+  },
+});
